08f614867762be0ba14bf099b08af3d811fef1b2
LGAFFF <xxxtentaciyt573@gmail.com>
2026-02-21 15:22:43 +0500

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -4,14 +4,10 @@ import { redirect, type LoaderFunctionArgs } from "@remix-run/cloudflare";
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const cookieHeader = request.headers.get("Cookie") || "";
-  
-  // Check if the user has the "is_allowed" cookie
   const isLogged = cookieHeader.includes("is_allowed=true");
 
-  // Don't redirect if they are already on the login page
   if (url.pathname === "/login") return null;
 
-  // If they aren't logged in, send them to the login page
   if (!isLogged) {
     return redirect("/login");
   }
