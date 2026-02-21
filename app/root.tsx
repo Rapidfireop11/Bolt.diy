efb07428acea06e1360483c78960c0d9e80f9c50
LGAFFF <xxxtentaciyt573@gmail.com>
2026-02-21 16:35:50 +0500

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,19 +1,4 @@
 import { Outlet, Scripts, ScrollRestoration, Meta, Links } from "@remix-run/react";
-import { redirect, type LoaderFunctionArgs } from "@remix-run/cloudflare";
-
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const url = new URL(request.url);
-  const cookieHeader = request.headers.get("Cookie") || "";
-  const isLogged = cookieHeader.includes("is_allowed=true");
-
-  if (url.pathname === "/login") return null;
-
-  if (!isLogged) {
-    return redirect("/login");
-  }
-
-  return null;
-};
 
 export default function App() {
   return (
